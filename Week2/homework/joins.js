477f592e314ffe01cbd6ac72c84bba1577e8679c
Enwer <52347489+erdem524@users.noreply.github.com>
2020-03-24 07:49:31 +0100

    Update joins.js

--- Week2/homework/joins.js
+++ Week2/homework/joins.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'hyfuser',
 	password: 'hyfpassword',
-	database: 'mydata'
+	database: 'academy'
 });
 const execQuery = util.promisify(connection.query.bind(connection));
 
