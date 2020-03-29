879dc089796176c3f02ba6838255a37519949b9e
enwer <tewbejan@gmail.com>
2020-03-29 09:40:55 +0200

    ex2 improvement

--- Week3/homework/Transactions/transaction.js
+++ Week3/homework/Transactions/transaction.js
@@ -1,9 +1,4 @@
-const util = require('util');
 const mysql = require('mysql');
-
-// const tr_insert_values = require('./tr-insert-values');
-// const account = tr_insert_values.account;
-// const account_changes = tr_insert_values.account_changes;
 
 const connection = mysql.createConnection({
 	host: 'localhost',
@@ -11,35 +6,55 @@ const connection = mysql.createConnection({
 	password: 'hyfpassword',
 	database: 'transact'
 });
-
-const execQuery = util.promisify(connection.query.bind(connection));
-
-async function transaction(acnt1, acnt2, transfer) {
-	const startTransaction = `start transaction;`;
-	const setCommit = `SET autocommit=0;`;
-
-	const sent = `update account  set balance = balance - ${transfer} where acnt_no = ${acnt1};`;
-	const received = `update account  set balance = balance + ${transfer} where acnt_no = ${acnt2};`;
-	const transferChange1 = `update account_changes  set amount = amount - ${transfer} where acnt_no = ${acnt1};`;
-	const transferChange2 = `update account_changes  set amount = amount + ${transfer} where acnt_no = ${acnt2};`;
-
-	const done = `commit;`;
-
-	connection.connect();
-
-	try {
-		// call the function that returns promise
-		await execQuery(startTransaction);
-		await execQuery(setCommit);
-		await execQuery(sent);
-		await execQuery(received);
-		await execQuery(transferChange1);
-		await execQuery(transferChange2);
-		await execQuery(done);
-	} catch (error) {
-		console.error(error);
+connection.beginTransaction(function(err) {
+	if (err) {
+		throw err;
 	}
-	connection.end();
-}
+	const transfer = 1000;
+	const acnt1 = 101;
+	const acnt2 = 102;
+	const queries = [
+		`update account set balance =  balance - ${transfer} where acnt_no = ${acnt1};`,
+		`update account set balance =  balance + ${transfer} where acnt_no = ${acnt2};`,
+		`update account_changes  set amount = amount - ${transfer} where acnt_no = ${acnt1};`,
+		`update account_changes  set amount = amount + ${transfer} where acnt_no = ${acnt2};`
+	];
+	connection.query(queries[0], function(error, results, fields) {
+		if (error) {
+			return connection.rollback(function() {
+				throw error;
+			});
+		}
+		connection.query(queries[1], function(error, results, fields) {
+			if (error) {
+				return connection.rollback(function() {
+					throw error;
+				});
+			}
+			connection.query(queries[2], function(error, results, fields) {
+				if (error) {
+					return connection.rollback(function() {
+						throw error;
+					});
+				}
+			});
 
-transaction(101, 102, 100);
+			connection.query(queries[3], function(error, results, fields) {
+				if (error) {
+					return connection.rollback(function() {
+						throw error;
+					});
+				}
+			});
+			connection.commit(function(err) {
+				if (err) {
+					return connection.rollback(function() {
+						throw err;
+					});
+				}
+				console.log('success!');
+			});
+			console.log(results);
+		});
+	});
+});
